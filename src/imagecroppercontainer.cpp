73852a2527712939da36aae4b54c7fc3a9fc9733
Bo <shine_5402@live.cn>
2018-10-31 17:01:44 +0800

    新增裁剪Widget的带缩放容器

---
+++ src/imagecroppercontainer.cpp
@@ -0,0 +1,44 @@
+﻿#include "imagecroppercontainer.h"
+#include "ui_imagecroppercontainer.h"
+
+ImageCropperContainer::ImageCropperContainer(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ImageCropperContainer)
+{
+    ui->setupUi(this);
+}
+
+ImageCropperContainer::~ImageCropperContainer()
+{
+    delete ui;
+}
+
+void ImageCropperContainer::setImage(const QPixmap &_image)
+{
+    ui->imageCropper->setImage(_image);
+}
+
+void ImageCropperContainer::setBackgroundColor(const QColor &_backgroundColor)
+{
+    ui->imageCropper->setBackgroundColor(_backgroundColor);
+}
+
+void ImageCropperContainer::setCroppingRectBorderColor(const QColor &_borderColor)
+{
+    ui->imageCropper->setCroppingRectBorderColor(_borderColor);
+}
+
+void ImageCropperContainer::setProportion(const QSizeF &_proportion)
+{
+    ui->imageCropper->setProportion(_proportion);
+}
+
+void ImageCropperContainer::setProportionFixed(const bool _isFixed)
+{
+    ui->imageCropper->setProportionFixed(_isFixed);
+}
+
+const QPixmap ImageCropperContainer::cropImage()
+{
+    return ui->imageCropper->cropImage();
+}
---
+++ src/imagecroppercontainer.h
@@ -0,0 +1,31 @@
+﻿#ifndef IMAGECROPPERWRAPPER_H
+#define IMAGECROPPERWRAPPER_H
+
+#include <QWidget>
+#include "imagecropper.h"
+namespace Ui {
+class ImageCropperContainer;
+}
+
+class ImageCropperContainer : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ImageCropperContainer(QWidget *parent = nullptr);
+    ~ImageCropperContainer();
+public slots:
+    void setImage(const QPixmap& _image);
+    void setBackgroundColor(const QColor& _backgroundColor);
+    void setCroppingRectBorderColor(const QColor& _borderColor);
+    void setProportion(const QSizeF& _proportion);
+    void setProportionFixed(const bool _isFixed);
+
+public:
+    const QPixmap cropImage();
+
+private:
+    Ui::ImageCropperContainer *ui;
+};
+
+#endif // IMAGECROPPERWRAPPER_H
